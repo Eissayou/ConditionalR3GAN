40918ff81789dc14c88ba8320284b0b0cb4b3591
Jason Eissayou <92198283+Eissayou@users.noreply.github.com>
2025-06-08 23:11:58 -0700

    Save fid50k snapshots with labels

--- gen_images.py
+++ gen_images.py
@@ -71,7 +71,7 @@ def generate_images(
     # Generate images.
     for seed_idx, seed in enumerate(seeds):
         print('Generating image for seed %d (%d/%d) ...' % (seed, seed_idx, len(seeds)))
-        z = torch.from_numpy(np.random.RandomState(seed).randn(1, G.z_dim)).to(device)
+        z = torch.from_numpy(np.random.RandomState(seed).randn(1, G.z_dim).astype(np.float32)).to(device)
         img = G(z, label)
         img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
         PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB').save(f'{outdir}/seed{seed:04d}.png')
